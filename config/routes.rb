a296c13a9501e284855e71bd62fff5c0dde2cb30
Marie Pierre Schmitz mpschmitz@hotmail.fr <mpschmitz@hotmail.fr>
2019-05-17 17:58:02 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "task", to: "task#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
